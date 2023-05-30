e8443736b3e23df3eacceced20246ed2c1c9c539
Simonsaidwhat475 <OfficialSimonGrome@gmail.com>
2023-05-30 11:58:00 -0400

    Fixed sample data

--- TempDataGenerator.py
+++ TempDataGenerator.py
@@ -5,18 +5,28 @@ import json
 
 response = requests.get("http://api.semanticscholar.org/graph/v1/paper/search?query=literature+graph")
 outfile = "SampleData.json"
+finalOutput = list()
 with open(outfile, "w") as filename:
     data = list()
+    print(response.json()['data'])
     data = response.json()['data']
     for item in data:
         id = item['paperId']
         newResponse = requests.get("http://api.semanticscholar.org/graph/v1/paper/" + str(id) + "?fields=title,citations,authors,year,abstract")
-        print(filename.write(json.dumps(newResponse.json()) + "\n"))
+        print(newResponse)
+        finalOutput.append(newResponse.json())
+        
+        
         recursiveData = newResponse.json()['citations']
         for item in recursiveData:
             recursiveId = item['paperId']
             recursiveNewResponse = requests.get("http://api.semanticscholar.org/graph/v1/paper/" + str(recursiveId) + "?fields=title,citations,authors,year,abstract")
-            print(filename.write(json.dumps(recursiveNewResponse.json()) + "\n"))
+            finalOutput.append(recursiveNewResponse.json())
+            print(recursiveNewResponse)
+    
+    #filename.write(json.dumps(finalOutput))
+    json.dump(finalOutput,filename)
+        
         
     
 
