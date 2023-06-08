8fcdf8409a1487fbe5c2a1a5fecd900535e32e75
AdamEshem <adameshemwork@gmail.com>
2023-06-08 02:50:35 -0400

    Created page that allows current users to login

--- infiniCite/urls.py
+++ infiniCite/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('paper.urls')),
-    path('paper/', include('paper.urls'))
+    path('paper/', include('paper.urls')),
+    path('user/', include('user.urls'))
 ]
