4c591bc6af116d14de9ff7ebd71462e9172f7cae
Wang-Zhongwei <jonathan_wang1999@icloud.com>
2023-06-10 16:09:14 -0400

    integrate bonsai

--- infiniCite/settings.py
+++ infiniCite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from config.django_config import SECRET_KEY
+from config.elastic_config import ELASTICSEARCH_DSL
+
+# configure elasticsearch
+ELASTICSEARCH_DSL = ELASTICSEARCH_DSL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
