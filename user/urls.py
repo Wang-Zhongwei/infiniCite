76b7e24ca5f317be492493a10679fb1fd7409e36
AdamEshem <adameshemwork@gmail.com>
2023-06-13 13:41:08 -0400

    Added user info in header of index.html, Added Logout Functionality

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns =[
     path('login/', views.user_login, name='login'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
