daace7f4fe31db711929b2d13929a3cbdd207225
AdamEshem <adameshemwork@gmail.com>
2023-06-13 16:30:07 -0400

    Removed my virtual environment from the project

--- paper/models.py
+++ paper/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 from django.conf import settings
-
-class Paper(models.Model):
-    users_saved = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='papers_saved', blank=True)
-    title = models.CharField(max_length=200)
-    url = models.URLField()
-    abstract = models.TextField()
