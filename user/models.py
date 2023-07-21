bd9d8272e7aab18a0b4be14f037322883d53701f
Niu-Jiaxuan <niujiaxuan134@gmail.com>
2023-07-21 16:53:12 -0400

    smart search bar almost done

--- user/models.py
+++ user/models.py
@@ -3,5 +3,10 @@ from django.contrib.auth.models import User
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    affiliation = models.CharField(max_length=255, blank=True, default='', null=True)
-    picture = models.ImageField(null=True, blank=True, upload_to='media')
+    affiliation = models.CharField(max_length=255, blank=True, default='')
+
+class SearchRecord(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, blank=False)
+    query = models.TextField(blank=False, null=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # TODO: add ispaper, isSemantic, isInLibrary
