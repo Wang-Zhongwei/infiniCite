2c20c64f0e7ba92d0fa766c9fb769a4deaceaf6f
Wang-Zhongwei <jonathan_wang1999@icloud.com>
2023-07-30 16:10:58 -0400

    concurrent requests

--- paper/management/commands/populate_incomplete_papers.py
+++ paper/management/commands/populate_incomplete_papers.py
@@ -2,6 +2,8 @@ import time
 from django.core.management.base import BaseCommand
 from paper.exceptions import SemanticAPIException
 from paper.services import PaperService
+import concurrent.futures
+import threading
 
 
 class Command(BaseCommand):
@@ -32,31 +34,44 @@ class Command(BaseCommand):
                 batch_size = 64
 
         if including_nested_fields:
-            incomplete_items_ids = self.paper_service.queryset.filter(references__isnull=True).values_list(
-                "paperId", flat=True
-            )
+            incomplete_items_ids = self.paper_service.queryset.filter(
+                references__isnull=True
+            ).values_list("paperId", flat=True)
         else:
-            incomplete_items_ids = self.paper_service.queryset.filter(publicationDate__isnull=True).values_list(
-                "paperId", flat=True
-            )
-        print(f'Found {len(incomplete_items_ids)} incomplete papers')
+            incomplete_items_ids = self.paper_service.queryset.filter(
+                publicationDate__isnull=True
+            ).values_list("paperId", flat=True)
+
+        print(f"Found {len(incomplete_items_ids)} incomplete papers that has empty {'references' if including_nested_fields else 'publicationDate'}")
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            future_to_paper_ids = {
+                executor.submit(
+                    self.update_papers, paper_ids, including_nested_fields
+                ): paper_ids
+                for paper_ids in [
+                    incomplete_items_ids[i : i + batch_size]
+                    for i in range(0, len(incomplete_items_ids), batch_size)
+                ]
+            }
+            for future in concurrent.futures.as_completed(future_to_paper_ids):
+                future.result()
+                   
+
+    def update_papers(self, paper_ids, including_nested_fields):
         time_to_sleep = 0.5
-        for i in range(0, len(incomplete_items_ids), batch_size):
-            paper_ids = incomplete_items_ids[i : i + batch_size]
-            while True:
-                try:
-                    self.paper_service.save_external_papers_by_ids(
-                        paper_ids, including_nested_fields
-                    )
-                    print(f"Successfully updated {len(paper_ids)} papers")
-                    time_to_sleep = 0.5
+        thread_name = threading.current_thread().name
+        while True:
+            try:
+                self.paper_service.save_external_papers_by_ids(paper_ids, including_nested_fields)
+                time_to_sleep = 0.5
+                print(f"{thread_name} successfully updated {len(paper_ids)} papers")
+                return
+            except SemanticAPIException as e:
+                if e.status_code == 429:
+                    print(f"{thread_name} sleeping for {time_to_sleep} seconds")
+                    time.sleep(time_to_sleep)
+                    time_to_sleep *= 2
+                    continue
+                else:
                     break
-                except SemanticAPIException as e:
-                    print(f"Failed to update {len(paper_ids)} papers due to {e}.")
-                    if e.status_code == 429:
-                        print(f"Sleeping for {time_to_sleep} seconds")
-                        time.sleep(time_to_sleep)
-                        time_to_sleep *= 2
-                        continue
-                    else:
-                        break
